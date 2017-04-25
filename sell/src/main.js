2a14f9566cb5f8fe0c5c0619ca0389f76d646fb5
yang-sir <57639850@qq.com>
2017-04-25 11:05:35 +0800

    add detail
    star组件    header完成

--- sell/src/main.js
+++ sell/src/main.js
@@ -11,10 +11,10 @@ Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-el: '#app',
-router,
-template: '<App/>',
-components: { App }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: { App }
 });
 router.push('/goods');
 
